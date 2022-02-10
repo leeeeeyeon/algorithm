cf1d39c66067917ef2b562e5f557a98a92a04aa7
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-02-10 14:45:22 +0900

    주석 추가

--- boj2910.py
+++ boj2910.py
@@ -2,7 +2,7 @@ a, b = input().split()
 
 arr = list(map(int, input().split()))
 res = list(set([(x, arr.count(x)) for x in arr]))
-order = list(dict.fromkeys(arr))
+order = list(dict.fromkeys(arr)) # 순서를 유지하면서 중복값 제거
 res.sort(key= lambda x:(-x[1], order.index(x[0])))
 
 ans = []
