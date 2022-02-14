31d25bc5c520f3ec00bea5b6b320f2f7cf2cdf37
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-02-14 12:40:31 +0900

    이친수 - 다른 사람 코드 및 피드백 추가

--- DP/boj2193.py
+++ DP/boj2193.py
@@ -9,3 +9,13 @@ for i in range(2, n):
     dp[i] = dp[i-1] + dp[i-2]
 
 print(dp[n-1])
+
+# 다른 사람 코드
+# s = [0, 1, 1]
+# for i in range(3, 91):
+#   s.append(s[i - 2] + s[i - 1])
+# n = int(input())
+# print(s[n])
+
+# 피드백
+# 점화식 : dp[i] = dp[i-1] + dp[i-2]
