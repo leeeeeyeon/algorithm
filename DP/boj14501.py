61d82e22533216d54f95c3bfdc79d00b6a68743b
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-02-18 16:31:13 +0900

    퇴사

--- DP/boj14501.py
+++ DP/boj14501.py
@@ -1,27 +1,63 @@
 # 내 코드
 n = int(input())
-# 더미데이터로 크기가 n인 리스트들을 만듦
-t = [-1] * n
-p = [-1] * n
-dp = [0] * n
+
+t = [0] * (n + 2)
+p = [0] * (n + 2)
+dp = [0] * (n + 2) # 0 ~ n + 1
 
 # t, p 입력 받음
-for i in range(n):
+for i in range(1, n + 1):
     t[i], p[i] = map(int, input().split())
 
-i = 1
+for i in range(n, 0, -1): # 7 6 5 4 3 2 1
+    if n - i + 1 - t[i] < 0:
+        dp[i] = dp[i + 1]
+    else:
+        dp[i] = max(p[i] + dp[i+t[i]], dp[i+1])
 
-for i in range(1, n+1):
-    print(f'[ i = {i} ]')
-    result = 0
-    temp = 0
-    while temp < i:
-        print(f'    temp = {temp}')
-        if t[temp] <= i - temp:
-            print(f'    t[{temp}](={t[temp]}) <= i - temp (={i-temp})')
-            result += p[temp]
-            temp += t[temp]
-        else:
-            temp += 1
-    
-        print(f'    result: {result}')
+print(dp[1])
+
+# 다른 사람 코드
+# #include <iostream>
+ 
+# using namespace std;
+ 
+# int t[1000] = { 0, };
+# int p[1000] = { 0, };
+# int dp[1000] = { 0, };
+ 
+# int max(int x, int y)
+# {
+#     return x > y ? x : y;
+# }
+ 
+# int main()
+# {
+#     int N;
+#     int next;
+ 
+#     cin >> N;
+ 
+#     for (int i = 1;i <= N;i++) {
+#         cin >> t[i] >> p[i];
+#     }
+ 
+#     for (int i = N;i > 0;i--) {
+#         next = i + t[i];
+#         if (next > N + 1) {
+#             dp[i] = dp[i + 1];
+#         }
+#         else {
+#             dp[i] = max(dp[i + 1], dp[next] + p[i]);
+#         }
+ 
+#     }
+ 
+#     cout << dp[1] << endl;
+ 
+#     return 0;
+# }
+
+# 피드백
+# - 참고: https://songsunbi.tistory.com/80
+# - 마지막 > 첫째 날 순으로 생각하면 더 쉽다.
