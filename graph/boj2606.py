b41b53b44b8d240b6d7ebed7879e3993a60656dd
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-06-21 15:33:00 +0900

    바이러스 - 문제 마저 품

--- graph/boj2606.py
+++ graph/boj2606.py
@@ -3,21 +3,58 @@ input = sys.stdin.readline
 
 a = int(input()) # 컴퓨터 수, 정점
 b = int(input()) # 컴퓨터 쌍의 수, 간선
-graph = [[] for i in range(a+1)]
-distance = [[] * (a+1)]
+graph = [[0 for _ in range(a+1)] for i in range(a+1)]
 visited = [False for i in range(a+1)]
+stack = []
+virus = []
 
 # 간선 정보 입력 받기
 for i in range(b):
     n, m = map(int, input().split())
-    graph[n].append(m)
+    graph[n][m] = 1
+    graph[m][n] = 1
 
-for elem in graph[1]:
-    visited[elem] = True
-    for i in graph[elem]:
-        if not visited[i]:
-           graph[1].append(i)
-           visited[elem] = True
+stack.append(1)
+virus.append(1)
+visited[1] = True
 
-print(graph)
-print(len(graph[1]))
+while len(stack) != 0:
+    top = stack[-1]
+    stack.pop()
+
+    for i in range(1, a+1):
+        if graph[top][i] == 1 and visited[i] == False:
+            stack.append(i)
+            virus.append(i)
+            visited[top] = True
+
+ans = len(list(set(virus))) -1
+print(ans)
+
+# for i in range(len(graph)):
+#     print(graph[i])
+
+
+# 다른 사람 코드
+# from sys import stdin
+# read = stdin.readline
+# dic={}
+# for i in range(int(read())):
+#     dic[i+1] = set()
+# for j in range(int(read())):
+#     a, b = map(int,read().split())
+#     dic[a].add(b)
+#     dic[b].add(a)
+
+# def dfs(start, dic):
+#     for i in dic[start]:
+#         if i not in visited:
+#             visited.append(i)
+#             dfs(i, dic)
+# visited = []
+# dfs(1, dic)
+# print(len(visited)-1)
+
+# 피드백
+# 나는 인접 행렬 방식을 사용했는데 파이썬에선 주로 인접 리스트를 사용
+# 반복문 대신 재귀를 사용하여 방문하지 않은 노드에 대해서만 DFS 적용
