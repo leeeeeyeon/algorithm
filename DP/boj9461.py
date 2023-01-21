563a020de431f0d28e25483268ec3e8de2def325
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-01-21 14:48:09 +0900

    파도반 수열

---
+++ DP/boj9461.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+dp = [0 for _ in range(101)]
+
+dp[1]=1
+dp[2]=1
+dp[3]=1
+dp[4]=2
+
+for i in range(5, 101):
+    dp[i] = dp[i-1] + dp[i-5]
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
