519fb24648007af26400a4d85635703ef0ff0842
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-01-24 11:19:43 +0900

    N과 M (1) - 백트래킹 코드 추가

--- 수학/boj15649.py
+++ 수학/boj15649.py
@@ -5,10 +5,30 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 
-num = [i for i in range(1, n+1)]
-p = permutations(num, m)
+# 백트래킹으로 푼 코드
+arr = [0 for i in range(m)]
+issued = [False for i in range(n+1)]
 
-for elem in p:
-    for e in elem:
-        print(e, end=' ')
-    print()
+def func(k):
+    if k == m:
+        for elem in arr:
+            print(elem, end=' ')
+        print()
+        return
+
+    for i in range(1, n+1):
+        if not issued[i]:
+            arr[k] = i
+            issued[i] = True
+            func(k+1)
+            issued[i] = False
+
+func(0)
+
+# 순열로 푼 코드
+# p = permutations(num, m)
+
+# for elem in p:
+#     for e in elem:
+#         print(e, end=' ')
+#     print()
