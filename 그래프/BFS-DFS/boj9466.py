55007615d0865a4a91afaf935560b6346fa991df
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-02-22 22:32:49 +0900

    텀 프로젝트

---
+++ 그래프/BFS-DFS/boj9466.py
@@ -0,0 +1,38 @@
+import sys
+sys.setrecursionlimit(10**5)
+
+input = sys.stdin.readline
+
+t = int(input())
+
+def dfs(x):
+    global result
+    visited[x] = True
+    cycle.append(x)
+    elem = graph[x][0]
+
+    if visited[elem]:
+        if elem in cycle: # 사이클을 이뤘을 때
+            result += cycle[cycle.index(elem):]
+        return
+    else:
+        dfs(elem)
+
+for _ in range(t):
+    n = int(input())
+    graph = [[] for _ in range(n+1)]
+
+    arr = [0] + list(map(int, input().split()))
+    visited = [False for _ in range(n+1)]
+    result = [] # 팀을 이룬 학생들
+
+    for i in range(1, n+1):
+        graph[i].append(arr[i])
+
+    for i in range(1, n+1):
+        if not visited[i]:
+            cycle = [] # 번호마다 사이클 여부를 판단하기 위해 초기화
+            dfs(i)
+
+    print(n-len(result))
+ 
