726bbdfac89886a61103b88c0761720aec491898
leeeeeyeon <leeeeeyeonn@gmail.com>
2023-03-02 16:03:58 +0900

    주식가격 - 다른 사람 코드 및 피드백 추가

--- 구현/pro42584.py
+++ 구현/pro42584.py
@@ -10,3 +10,23 @@ def solution(prices):
     return answer
 
 print(solution([1, 2, 3, 2, 3]))
+
+# 다른 사람 코드
+# def solution(prices):
+#     answer = [0]*len(prices)
+#     stack = []
+ 
+#     for i, price in enumerate(prices):
+#         while stack and price < prices[stack[-1]]:
+#             j = stack.pop()
+#             answer[j] = i - j
+#         stack.append(i)
+
+#     while stack:
+#         j = stack.pop()
+#         answer[j] = len(prices) - 1 - j
+
+#     return answer
+
+# 피드백
+# 완전 탐색으로도 문제를 풀 수 있지만 스택이나 큐를 사용하여 효율성을 높일 수 있다
